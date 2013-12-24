86e6ac81d7439af3da1574664e503de9b1fad328
RandomOutput <dpx109@gmail.com>
2013-12-24 16:32:31 -0500

    initial commit

---
+++ falalalabot.py
@@ -0,0 +1,93 @@
+import twitter
+import time
+import configs
+import sys
+import re
+import random
+
+class FaLaLaLaBot:
+
+	def __init__(self):
+		print 'Starting FaLaLaLaBot'
+
+		self.nextPushTime = 0
+		self.pushInterval = 60 * 60
+		self.failureInterval = 60 * 2 #wait two minutes on failure and try again
+		self.result_type = 'recent'
+		self.usedStrings = list()
+
+		try:
+			self.api = twitter.Api(consumer_key=configs.consumer_key, consumer_secret=configs.consumer_secret, access_token_key=configs.access_token_key, access_token_secret=configs.access_token_secret)
+			self.api.VerifyCredentials()
+		except twitter.TwitterError as e:
+			print "Twitter Error: ", e.message
+			sys.exit(0)
+		except:
+			print "Unexpected error in API authentication: ", sys.exc_info()[0]
+			sys.exit(0)
+
+		#self.makePost()
+		
+		#self.makePost()
+
+		while(True):
+			if time.time() >= self.nextPushTime:
+				self.makePost()
+
+	def makePost(self):
+		
+		noGo = 'jolly|fa |la |tra '
+
+		for used in self.usedStrings:
+			noGo += '|' + str(used)
+
+		try:
+			results = self.api.GetSearch(term='tis the season to', result_type=self.result_type)
+		except twitter.TwitterError as e:
+			print "Twitter Error: ", e.message 
+			sys.exit(0)
+		except:
+			print "Unexpected error in twitter search: ", sys.exc_info()[0]
+			sys.exit(0)
+
+		resultText = list()
+		regexedList = list()
+
+		for r in results:
+			resultText.append(r.text)
+		#	print "\n", r.text
+
+		#print "------"
+
+		for t in resultText:
+			reged = re.search('(Tis the season to be )([a-zA-Z][a-zA-Z -]+[a-zA-Z])', t)
+
+			if(reged != None and re.search(noGo, str(reged.group(2))) == None):
+				regexedList.append(str(reged.group(2)))
+		
+		while(True):
+			if(len(regexedList) > 0):
+				toBeString = regexedList[random.randint(0,len(regexedList)-1)]
+				post = str("'Tis the season to be " + toBeString + ". Falalalala. Lalalala.")
+
+				if(len(post) <= 140):
+					self.pushPost(post)
+					self.usedStrings.append(toBeString)
+					break
+			else:
+				print "Error: No valid tweets"
+				self.nextPushTime = time.time() + failureInterval
+				break
+
+	def pushPost(self, post):
+		try:
+			self.nextPushTime = time.time() + self.pushInterval
+			status = self.api.PostUpdate(str(post))
+			print "\n" + str(time.time()), "POSTED: ", status.text
+			print "Next Push at: " + str(self.nextPushTime) + "\n"
+		except:
+			print "Unexpected error in push: ", sys.exc_info()[0]
+			self.nextPushTime = time.time() + failureInterval
+
+if __name__ == '__main__':
+	bot = FaLaLaLaBot()
---
+++ readme.md
@@ -0,0 +1,9 @@
+#FaLaLaLaBot 
+##A Twitter Bot by Daniel Plemmons
+
+FaLaLaLaBot is a very simple twitter bot that find tweets once an hour where people say "Tis the season to be..."  and reposts it with a FaLaLaLaLa. It trys to avoid things its said before (though isn't great at it.)
+
+*Additional Requirements:*
+- python-twitter: https://github.com/bear/python-twitter.
+- A twitter account with an active application attached.
+- a config.py with your login credentials.
